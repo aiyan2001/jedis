b03c5d170bcb311a79761f40525ce95febdac97a
aiyan <aiyan2001@sohu.com>
2015-04-14 10:10:29 +0800

    hehe

--- src/main/java/redis/clients/jedis/JedisCluster.java
+++ src/main/java/redis/clients/jedis/JedisCluster.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-//aiyan2001_1
+//aiyan2001_1_2
 
 
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
